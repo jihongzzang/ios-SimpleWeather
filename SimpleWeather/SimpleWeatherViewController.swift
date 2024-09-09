0a70cd5ed1c2202f25fedb822d28cd93b0bf97fa
jujihong <jujihong2@gmail.com>
2024-09-09 19:24:43 +0900

    SimpleWeather UI 구성

---
+++ SimpleWeather/SimpleWeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SimpleWeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by 주지홍 on 9/9/24.
+//
+
+import UIKit
+
+class SimpleWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
